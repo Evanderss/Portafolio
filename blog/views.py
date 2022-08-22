53bba1cdc80cb3460ed54eff6d94cb649edbd2a5
Evanderss <evanderramirez150@gmail.com>
2022-08-22 17:10:08 -0500

    modificando post.html y poniendo el primer post

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
 def render_post(request):
-    return render(request, "post.html")
+    posts = Post.objects.all()
+    return render(request, "post.html", {"posts": posts})
     
